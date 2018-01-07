55bcd0426602503dee3c90c599a1431f47c2c13f
yky <ykycome00#naver.com>
2018-01-07 12:17:33 +0900

    [20180107] 카카오알고리즘 모의테스트 2번문제

---
+++ algorithm/src/giyeol/Solution.java
@@ -0,0 +1,63 @@
+package giyeol;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		boolean answer = true;
+		
+		//test case 1,2
+		//int[] num = {4,1,3};
+		int[] num = {4,1,3,2};
+		
+		/*Scanner scan = new Scanner(System.in);
+		
+		int n = -1;
+		
+		while(n>100000 || n<0) {
+			n = scan.nextInt();
+		}
+		
+		int[] num = new int[n];
+		
+		for(int i = 0; i<num.length; i++) {
+			num[i] = scan.nextInt();
+		}*/
+		
+		//배열 오름차순 정렬
+		int temp = 0;
+		for(int i=0; i<num.length; i++) {
+			for(int j=i+1; j<num.length; j++) {
+				if(num[i] > num[j]) {
+					temp = num[i];
+					num[i] = num[j];
+					num[j] = temp;
+				}
+			}
+		}
+		
+		answer = solution(num); //함수호출
+		
+		System.out.println(answer);
+		
+
+	}
+	
+	
+	//1~n 체크 메소드
+	public static boolean solution(int[] arr) {
+        boolean answer = true;
+        int count = 1;
+		for(int i=0; i<arr.length; i++) {
+			if(count != arr[i]) {
+				answer = false;
+				break;
+			}
+			count++;
+		}
+		 return answer;
+    }
+
+}
