7a172d981b5cb69115eca5086856d8ff3f4070ee
yky <ykycome00#naver.com>
2018-01-07 23:39:17 +0900

    3

---
+++ algorithm/src/giyeol/Solution03.java
@@ -0,0 +1,28 @@
+package giyeol;
+
+public class Solution03 {
+
+	public static void main(String[] args) {
+		
+		int[][] num =  {{0,0,1,1},{1,1,1,1}};
+	
+		
+		//정사각형이 되기 위한 조건
+		//제일큰 정사각형 좌표를 만들어서 체크
+		
+		int rect = 0;
+		
+		
+		//4*4만들어서체크
+		//3*3만들어서 전부체크...
+		for(int i=0; i<num.length; i++) {
+			for(int j=0; j<num.length; j++) {
+				
+			}
+		}
+		
+		
+	
+	}
+
+}
