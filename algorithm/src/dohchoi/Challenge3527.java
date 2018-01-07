d2141b262851ec96a7be95399358e7734890df2b
dohchoi <dhyun175@gmail.com>
2018-01-07 13:13:48 +0900

    [20180107] 카카오알고리즘 모의테스트 2,3번문제

--- algorithm/src/dohchoi/Challenge3527.java
+++ algorithm/src/dohchoi/Challenge3527.java
@@ -12,7 +12,7 @@
 package dohchoi;
 
 import java.util.stream.IntStream;
-public class Challenges3527 {
+public class Challenge3527 {
 	public static void main(String args[]) {
     	int[] arr = {1,2,3,4};
     	
