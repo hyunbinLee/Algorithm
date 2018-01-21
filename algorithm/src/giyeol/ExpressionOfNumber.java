09dcd366c3530eb7baac00dc5700f3bb45f29507
yky <ykycome00#naver.com>
2018-01-21 12:45:19 +0900

    [20180121] 이클립스 숫자의표현

--- algorithm/src/giyeol/ExpressionOfNumber.java
+++ algorithm/src/giyeol/ExpressionOfNumber.java
@@ -8,6 +8,10 @@ public class ExpressionOfNumber {
 		
 		System.out.println(testAnswer);
 		
+		Expressions expressions = new Expressions();
+		// 아래는 테스트로 출력해 보기 위한 코드입니다.
+		System.out.println(expressions.expressions(15));
+		
 	}
 	
 	public static int expressions(int testCase)
