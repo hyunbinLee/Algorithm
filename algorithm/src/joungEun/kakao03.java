9d5a2eda06965f97e0d64a23802e468d298cd3df
JoungEunLee <amy1123kr@gmail.com>
2018-02-18 12:56:02 +0900

    2018 카카오테스트03 캐시

---
+++ algorithm/src/joungEun/kakao03.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+public class kakao03{
+	public static void main(String args[]){
+		solution(3, new String[]{"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"});
+			
+	}
+	
+	public static void solution(int cachesize, String[] cities){
+		LinkedList<String> cache = new LinkedList<String>();
+		int time = 0;
+		
+		for(int i = 0; i < cities.length; i++){
+			String city = cities[i].toUpperCase();
+			
+			if (cache.contains(city)){
+				cache.remove(city);
+				cache.add(city);
+				time+=1;
+				System.out.println(cache);
+			} else {
+				//cache.remove(0);
+				cache.add(city);
+				time+=5;
+			}
+			
+		}
+		
+		//캐시사이즈 인식X 똑같은 글자가 나오면 remove하고 add한거만 인식
+		
+		
+	}
+}
